3d357d581232ee2a2e1135f68df36a7323ce7e58
Thomas Lewis <tplewis@gmail.com>
2019-09-09 10:52:05 -0400

    added models for - Article  - Note

---
+++ model/Article.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  link: {
+    type: String,
+    required: true
+  },
+
+  summary: {
+    type: String
+  },
+
+  byLine: {
+    type: String
+  },
+
+  isSaved: {
+    type: Boolean,
+    default: false
+  },
+  // link to the Note collection
+  note: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'Note'}],
+  }
+});
+
+var Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article; //export the Article model
